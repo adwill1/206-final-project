eada2227294d0b5ae7d8b14b997851be0d5080b9
laurenfulcher <lfulcher@umich.edu>
2021-12-06 21:57:24 -0500

    Add files via upload

---
+++ covidapi.py
@@ -0,0 +1,21 @@
+
+# SI 206 Final Project- Covid API
+# Name: 
+
+import json
+import unittest
+import os
+import requests
+import matplotlib
+
+
+#functions
+
+def main():
+    print("This is main")
+    pass
+
+main()
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
