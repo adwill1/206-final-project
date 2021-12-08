46fea94512f5a74db155d65f0ed72a9cf79f49cc
Sarah Whitman <sarahwhitman@0587373816.wireless.umich.net>
2021-12-08 13:41:01 -0500

    get_cases function

--- covidapi.py
+++ covidapi.py
@@ -16,32 +16,42 @@ def get_countries_from_api():
     dict_list = json.loads(data)
 
     country_list = list(dict_list.keys()) 
-    print(country_list)
-    print(len(country_list))
+    #print(country_list)
+    #print(len(country_list))
     return country_list
 
-def get_region_from_api():
+def get_cases_from_api():
     url = "https://covid-api.mmediagroup.fr/v1/cases"
     r = requests.get(url)
     data = r.text
     dict_list = json.loads(data)
+    
+    cases_list = []
+    for country_key in dict_list:
+        cases = dict_list[country_key]["All"]["confirmed"]
+        cases_list.append(cases)
+    print(cases_list)
+    print(len(cases_list))
+    return cases_list
 
-    region_list = []
-    for country in dict_list:
-        
+def get_deaths_from_api():
 
 
 
 
 class TestCases(unittest.TestCase):
-    def country_list_length(self):
+    def test_get_countries_from_api(self):
         country_list = get_countries_from_api()
         self.assertEqual(len(country_list), 197)
-
+    
+    def test_get_cases_from_api(self):
+        cases_list = get_cases_from_api()
+        self.assertEqual(len(cases_list), 197)
 
 def main():
     print("This is main")
     get_countries_from_api()
+    get_cases_from_api()
 
 main()
 if __name__ == '__main__':
