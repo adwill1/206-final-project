fd5ce414d0ba89c8bcc59b8b1bd484f5e8b122f1
Sarah Whitman <sarahwhitman@0587373816.wireless.umich.net>
2021-12-08 13:30:22 -0500

    get_countries function

--- covidapi.py
+++ covidapi.py
@@ -1,6 +1,6 @@
 
 # SI 206 Final Project- Covid API
-# Name: 
+# Name: Sarah 
 
 import json
 import unittest
@@ -8,12 +8,40 @@ import os
 import requests
 import matplotlib
 
-
 #functions
+def get_countries_from_api():
+    url = "https://covid-api.mmediagroup.fr/v1/cases"
+    r = requests.get(url)
+    data = r.text
+    dict_list = json.loads(data)
+
+    country_list = list(dict_list.keys()) 
+    print(country_list)
+    print(len(country_list))
+    return country_list
+
+def get_region_from_api():
+    url = "https://covid-api.mmediagroup.fr/v1/cases"
+    r = requests.get(url)
+    data = r.text
+    dict_list = json.loads(data)
+
+    region_list = []
+    for country in dict_list:
+        
+
+
+
+
+class TestCases(unittest.TestCase):
+    def country_list_length(self):
+        country_list = get_countries_from_api()
+        self.assertEqual(len(country_list), 197)
+
 
 def main():
     print("This is main")
-    pass
+    get_countries_from_api()
 
 main()
 if __name__ == '__main__':
